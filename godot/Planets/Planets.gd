6886357b15bcd1aa8a060de78f6d2b6d1345d0fa
DoriDie <torridori@web.de>
2025-04-05 21:41:46 +0200

    planeten Varianten

---
+++ godot/Planets/Planets.gd
@@ -0,0 +1,42 @@
+extends Node
+
+@export var extents: float
+@export var numPlanets: int
+@export var minDist: float
+var planet =  preload("res://planet.tscn")
+
+var points = []
+var rng = RandomNumberGenerator.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    for i in range(numPlanets):
+        var x = rng.randf_range(-extents, extents)
+        var y = rng.randf_range(-extents, extents)
+        var point = Vector2(x, y)
+        points.append(point)
+        
+    var toRemove = []
+    var i = 0
+    while i < points.size():
+        var popped = 0
+        for j in range(points.size()-1, 0, -1):
+            if j < i:
+                if points[i-popped].distance_to(points[j]) < minDist:
+                    points.pop_at(j)
+                    popped += 1
+            else:
+                if i != j and points[i].distance_to(points[j]) < minDist:
+                    points.pop_at(j)
+        i += 1
+        
+    for p in points:
+        var instance = planet.instantiate()
+        instance.position = p
+        add_child(instance)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
--- godot/Planets/base_planet_asset.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-    set_clip_children_mode(CanvasItem.CLIP_CHILDREN_ONLY)
