5371b4952786a6d574ab0093ecd13dfd07e73a09
Robin Schmitz <schmitz.robin@gmx.de>
2025-04-07 22:28:19 +0200

    dynamic quest texts

---
+++ godot/content/QuestTexts.gd
@@ -0,0 +1,8 @@
+class_name Quest_Texts extends Node
+
+@export var main_text_quest_1: String
+@export var sub_text_quest_1: String
+@export var main_text_quest_2: String
+@export var sub_text_quest_2: String
+@export var main_text_quest_3: String
+@export var sub_text_quest_3: String
--- godot/scripts/GameUi.gd
+++ godot/scripts/GameUi.gd
@@ -1,17 +1,27 @@
 extends Control
 class_name GameUi
 
+
 @export var gradient: Gradient = Gradient.new()
 @export var color_by_energy_level: bool = false
 @export var energy_gradient: Gradient
+@export var quest_texts: Quest_Texts
+
 @onready var o_label: Label = $UIObenRechts/Panel/HBoxContainer/O_BoxContainer/PointCount
 @onready var c_label: Label = $UIObenRechts/Panel/HBoxContainer/C_BoxContainer/PointCount
 @onready var h_label: Label = $UIObenRechts/Panel/HBoxContainer/H_BoxContainer/PointCount
 @onready var energy_label: Label = $UIObenRechts/Panel2/HBoxContainer/PointCount
+@onready var txt_quest_main: Label = $UIObenRechts/Panel3/txt_quest_main
+@onready var txt_quest_sub: Label = $UIObenRechts/Panel3/txt_quest_sub
+
 @onready var help_button: Button = $UIObenLinks/Button
 
 func _ready() -> void:
 	GameManager.on_ship_collectibles.connect(_on_ship_collectibles)
+	GameManager.stage_changed.connect(_on_quest_state_changed)
+	
+	txt_quest_main.text = quest_texts.main_text_quest_1
+	txt_quest_sub.text = quest_texts.sub_text_quest_1
 	_on_ship_collectibles(GameManager.collectibles_on_ship)
 	help_button.pressed.connect(_on_help_button)
 	pass # Replace with function body.
@@ -36,7 +46,24 @@ func _on_ship_collectibles(collectibles: Dictionary) -> void:
 		elif key == GameManager.Chem.ChemN:
 			pass
 			# Handle Nitrogen if needed
-			
+
+func _on_quest_state_changed(_stage: int):
+	match _stage:
+		0: 
+			txt_quest_main.text = quest_texts.main_text_quest_1
+			txt_quest_sub.text = quest_texts.sub_text_quest_1
+			pass
+		1: 
+			txt_quest_main.text = quest_texts.main_text_quest_2
+			txt_quest_sub.text = quest_texts.sub_text_quest_2
+			pass
+		2: 
+			txt_quest_main.text = quest_texts.main_text_quest_3
+			txt_quest_sub.text = quest_texts.sub_text_quest_3
+			pass
+		pass
+	pass
+
 func fmt(current, goal):
 	return str(int(ceil(current))) + "/" + str(int(floor(goal)))
 
